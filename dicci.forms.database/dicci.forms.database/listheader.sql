a3237ef8e60fe83dd31c21684017203da63b57fa
Richard Lee <ric@dicci.co.uk>
2019-03-21 15:44:52 +0000

    Initial project check-in

---
+++ dicci.forms.database/dicci.forms.database/listheader.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[listheader]
+(
+	[ID] INT IDENTITY NOT NULL PRIMARY KEY, 
+    [guid] UNIQUEIDENTIFIER NULL, 
+    [status] SMALLINT NOT NULL DEFAULT 1, 
+    [name] NCHAR(50) NOT NULL, 
+    [type] NCHAR(100) NOT NULL
+)
